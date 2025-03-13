8cd8f331a0e4efefb16c9fdb7a951225c7793535
sameer-panwar <sameerpanwar9720@gmail.com>
2025-03-14 02:35:58 +0530

    Done with login part

--- frontend/src/Components/loginpage/Login.jsx
+++ frontend/src/Components/loginpage/Login.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
+import { loginSchema, signupSchema } from '../type';
 import axios from "axios";
 
 
@@ -35,28 +36,47 @@ export function Login() {
 const Template = ({ title, buttonText ,nextPage, onClick, btnColor, showUsername = true}) => {
     const navigate = useNavigate();
     const [message, setMessage]= useState("");
-    const [loginState, setLoginState]=useState("notFilling");
+    const [msgColor, setMsgColor]= useState("");
+    const [loginState, setLoginState]=useState("");
+    const [errors, setErrors]=useState({});
+    const [submitted, setSubmitted]=useState(false);
     const [formData, setFormData] = useState({
         username: "",
         email: "",
         password: "",
     });
 
+    
+
     const handleLoginState = () => {
         
-        setMessage("Logging...")
-
-        if(title === "Login"){
-            setMessage("Login Successfull");
+        if(title == "Sign Up"){
+            setMsgColor("text-green-400");
+            setMessage("creating your account...");
         }else{
-            setMessage("Your new account is Created!")
+            setMsgColor("text-green-400");
+            setMessage("Logging...");
         }
-        // setFormState("Logging...");
-    
-        // setTimeout(() => {
-        //     setFormState("Login Successfull");
-        //     setLoginState("Fullfilled");
-        // }, 3000);
+
+        setTimeout(() => {
+            setLoginState((prevLoginState) => {
+                if (prevLoginState === "fullfilled") {
+                    console.log("fullfilled");
+                    setMsgColor("text-green-400");
+                    setMessage(title === "Sign Up" ? "Your Account is created!" : "Login successful!");
+        
+                    setTimeout(() => {
+                        navigate('/homePage');
+                    }, 1000);
+                } else {
+                    console.log("no no no notfullfilled");
+                    setMsgColor("text-red-500");
+                    setMessage(title === "Sign Up" ? "Invalid Email/Password" : "Login Failed, Try again!");
+                }
+                return prevLoginState;
+            });
+        }, 3000);
+        
     };
 
 
@@ -67,47 +87,69 @@ const Template = ({ title, buttonText ,nextPage, onClick, btnColor, showUsername
           }));
     };
 
+    useEffect(()=>{
+        if(submitted){
+            setErrors({});
+            setSubmitted(false);    
+        }
+    },[formData]);
+
     const handleLogin = async (e) => {
         e.preventDefault();
+        setSubmitted(true);
 
-        console.log('flsldslfjds');
-        try {
-            if(title == "Sign Up"){
-                const response = await axios.post("http://localhost:3000/signup", formData, {
-                    headers: { "Content-Type": "application/json" },
-                });
-                localStorage.removeItem("token");
-                localStorage.setItem("token", response.data.token);
-                console.log("User created successfully");
-                handleLoginState();
-                navigate('/homePage');
-                
-            }else if(title == "Login"){
-                const response = await axios.post("http://localhost:3000/login", formData, {
-                    headers: { "Content-Type": "application/json" }
-                });
-
-                console.log(response.data);
-                console.log(response.data.success);
-                console.log(response.data.msg);
-
-                
-                if(response.data.success){
+        const result = (title === "Sign Up") ? signupSchema.safeParse(formData) : loginSchema.safeParse(formData);
+    
+        console.log("ider se toh nikal chuke hai");
+        if(!result.success) {
+            const errorsObject=result.error.format();
+            setErrors(errorsObject);
+            console.log(errorsObject);
+            setTimeout(() => { 
+                console.log("After setting errors... still running");
+            }, 1000); 
+            console.log(result.success);
+        }else{
+            setErrors({});
+            console.log("Moving to next step...");
+            try {
+                if(title == "Sign Up"){
+                    const response = await axios.post("http://localhost:3000/signup", formData, {
+                        headers: { "Content-Type": "application/json" },
+                    });
                     localStorage.removeItem("token");
                     localStorage.setItem("token", response.data.token);
-                    console.log("Login successfully");
+                    console.log("User created successfully");
+                    setLoginState("fullfilled");
                     handleLoginState();
-                    navigate('/homePage');
                     
-                }else{
-                    console.log("not found");
-                }
+                }else if(title == "Login"){
+                    const response = await axios.post("http://localhost:3000/login", formData, {
+                        headers: { "Content-Type": "application/json" }
+                    });
+                    console.log("Sending to backend:", formData);
 
+                    
+                    if(response.data.success){
+                        localStorage.removeItem("token");
+                        localStorage.setItem("token", response.data.token);
+                        setLoginState("fullfilled");
+                        console.log("dekh bhai set krra hu setloginstate ko fullfilled");
+                        handleLoginState();
+                        console.log("Login successfull");
+                        
+                    }else{
+                        console.log("not found");
+                        setLoginState("notfullfilled");
+                        handleLoginState();
+                    }
+
+                }
+            } catch (error) {
+                setLoginState("notfullfilled");
+                handleLoginState ();
+                console.error("Something went wrong:", error);
             }
-        } catch (error) {
-            setLoginState("notFullfilled");
-            setMessage("Invalid Email/Password");
-            console.error("Something went wrong:", error);
         }
         
         
@@ -127,6 +169,7 @@ const Template = ({ title, buttonText ,nextPage, onClick, btnColor, showUsername
                             name="username"
                             value={formData.username}
                             onChange={handleChange}
+                            showErrors={errors.username && errors.username._errors[0]}
                         />
                     )}
                     <Label
@@ -136,12 +179,14 @@ const Template = ({ title, buttonText ,nextPage, onClick, btnColor, showUsername
                         name="email"
                         value={formData.email}
                         onChange={handleChange}
+                        showErrors={errors.email && errors.email._errors[0]}
                     />
                     <Label
                         children="Password"
                         placeholder="hkkklnkbk"
                         type="password"
-                        condition="Must be at least 6 characters long."
+                        condition="Must be atleast 6 Charaters long"
+                        showErrors={errors.password && errors.password._errors[0]}
                         name="password"
                         value={formData.password}
                         onChange={handleChange}
@@ -153,7 +198,7 @@ const Template = ({ title, buttonText ,nextPage, onClick, btnColor, showUsername
                     >
                         {buttonText}
                     </button>
-                    <div className='text-center mt-2 font-bold text-green-400'>{message}</div>
+                    <div className={`text-center mt-2 font-bold text-sm ${msgColor}`}>{message}</div>
                 </form>
 
                 <div className="place-self-center font-medium my-4 text-sm" onClick={onClick}>
@@ -168,12 +213,16 @@ const Template = ({ title, buttonText ,nextPage, onClick, btnColor, showUsername
 };
 
 
-function Label({ children, placeholder, type, condition, name, value, onChange}) {
+function Label({ children, placeholder, type, condition, name, value, onChange, showErrors}) {
     return (
         <div className="pb-6 flex flex-col">
             <label className="font-medium mb-1" htmlFor={children}>{children}</label>
             <input className="h-10 p-2 outline-dashed outline-gray-400 outline-1" placeholder={placeholder} type={type} name={name} value={value} onChange={onChange}/>
-            <div className="pt-1 text-xs font-medium text-gray-500">{condition}</div>
+            {showErrors?
+                <div className="pt-1 text-xs font-bold text-red-500">{showErrors}</div>
+                :
+                <div className='pt-1 text-xs font-medium text-gray-500'>{condition}</div>
+            }
         </div>
     )
 }
