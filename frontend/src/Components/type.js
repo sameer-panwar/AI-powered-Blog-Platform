97ca95e320300b32d8e16101b308732139813cb2
sameer-panwar <sameerpanwar9720@gmail.com>
2025-03-14 02:35:10 +0530

    created a validation zod file for frontend

---
+++ frontend/src/Components/type.js
@@ -0,0 +1,22 @@
+import {z} from 'zod';
+
+export const signupSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters"),
+    email: z.string().nonempty("email must not be empty").email("Invalid email format"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+  });
+  
+  export const loginSchema = z.object({
+    email: z.string().nonempty("email must not be empty").email("Invalid email format"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+  });
+
+
+export const blogCheck=z.object({
+    title: z.string().min(3, "Title should be atleast 3 character long"),
+    content: z.string(),
+    keywords: z.array(z.string()).min(1, "Atleast 1 keyword is required"),
+    name: z.string(),
+    role: z.string(),
+    likes: z.number()
+})
