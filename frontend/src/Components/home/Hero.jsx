712c2b9d29d9b6c8cf3b0bbeb5fcff856a09a865
sameer-panwar <sameerpanwar9720@gmail.com>
2025-03-16 11:44:51 +0530

    added auto login functionality on hero.jsx page

--- frontend/src/Components/home/Hero.jsx
+++ frontend/src/Components/home/Hero.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import { Contact } from "./pages/Contact";
 import { Features } from "./pages/Features";
 import { Home } from "./pages/Home";
 import { Resources } from "./pages/Resource";
+import { useNavigate } from "react-router-dom";
 
 export function Hero(){
+    const navigate=useNavigate();
+    useEffect(async ()=>{
+        try{const response=await fetch("http://localhost:3000/autoLogin",{
+            headers:{
+                authorization: localStorage.getItem("token")
+            }
+        })
+        console.log(response.status);
+        navigate("/homePage");
+        }catch(error){
+            console.log("Server Error", error); 
+        }
+    },[])   
     return(
         <>
             <Home/>
