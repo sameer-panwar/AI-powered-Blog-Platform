9e5db9b968f7e7107a7806b438b55ffceef355e8
sameer-panwar <sameerpanwar9720@gmail.com>
2025-04-26 17:20:57 +0530

    Created new directory for middlewares

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const userDB = require("../models/User");
+
+const secretkey="2400424";
+
+const generateToken = (createPayload, secretkey)=>{
+    const token= jwt.sign({email: createPayload.email, password: createPayload.password}, secretkey);
+    return token;
+}
+
+const verifyToken=async (req, res, next)=>{
+    const token=req.headers.authorization;
+
+    if (!token) {
+        console.log("inalid token");
+        return res.status(401).json({ error: "No token provided" }); 
+    }
+
+    jwt.verify(token, secretkey,async (err, decoded)=>{
+        if(err){
+            console.log("not valid token");
+            return res.status(401).json({error: "Invalid token"})
+        }
+
+        req.user =await userDB.findOne({email: decoded.email});
+        next();
+
+    })
+}
+
+module.exports = {
+    generateToken,
+    verifyToken
+}
