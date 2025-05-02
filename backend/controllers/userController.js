3c444de498b23393ed72ecf5788eeb27827a7bd7
sameer-panwar <sameerpanwar9720@gmail.com>
2025-05-02 15:25:43 +0530

    Fixed edit profile fucntion

---
+++ backend/controllers/userController.js
@@ -0,0 +1,73 @@
+const userDB = require("../models/User");
+const blogsDB = require("../models/Blog");
+
+exports.profile = async (req, res)=>{
+
+    const email=req.user.email;
+    const user=await userDB.findOne({email});
+    if(!user){
+        return res.status(404).json({msg: "Data not Found"});
+    }
+
+    res.status(200).json({msg: "Here is your profile", success: true, user})
+}
+
+
+exports.adminBlogs = async(req, res)=>{
+    const email=req.user.email;
+
+    const user=await userDB.findOne({email});
+    
+    const {username} =user;
+
+    const adminBlogs=await blogsDB.find({username});
+
+    res.status(200).json({
+        msg: "Here are the Admin Blogs",
+        success: true,
+        adminBlogs
+    })    
+}
+
+exports.editProfile = async (req, res) => {
+    try {
+        console.log("im in editprofile");
+        const {id, name, role, bio } = req.body;
+        const username = req.user.username;
+
+        console.log(id, name, role, bio, username);
+
+        if (!name|| !bio || !role) {
+            return res.status(400).json({ msg: "All fields are required" });
+        }
+
+        const updatedUser = await userDB.findByIdAndUpdate(
+            id, 
+            { name, role, bio },
+            { new: true }
+        );
+        
+        console.log("updated User: ", updatedUser);
+
+        const updatedBlogs= await blogsDB.updateMany(
+            {username: username},
+            {$set: {name: name , role: role}}
+        );
+
+        console.log("updated blogs");
+
+        if (!updatedUser) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+
+        res.status(200).json({
+            msg: "Profile changes have been saved",
+            success: true,
+            user: updatedUser,
+            blogs: updatedBlogs
+        });
+    } catch (error) {
+        console.error("Error updating profile:", error);
+        res.status(500).json({ msg: "Internal Server Error" });
+    }
+}
--- frontend/src/Components/Dashboard/pages/Profile.jsx
+++ frontend/src/Components/Dashboard/pages/Profile.jsx
@@ -1,7 +1,102 @@
 import { useEffect, useState } from "react";
+import { Label } from "../../Auth/Login"
 import axios from 'axios';
-import { EditProfile } from "./EditProfile";
 import { Heart ,MessageCircle} from "lucide-react";
+
+const EditProfile=({profile, onClose})=>{
+    const [newValues, setNewValues]=useState({});
+    const [success, setSuccess]=useState(false);
+
+    useEffect(()=>{
+        const id=profile._id
+        const name=profile.name;
+        const role=profile.role;
+        const bio=profile.bio;
+        setNewValues({
+            id: id,
+            name: name,
+            role: role,
+            bio: bio
+        });
+    },[]);
+
+    function handleChange(e){
+        setNewValues((prev)=>({
+            ...prev,
+            [e.target.name] : e.target.value 
+        }))
+    }
+    
+    const handleSubmit=async ()=>{
+        console.log(newValues);
+        const id=  localStorage.getItem("userID");
+        
+        try{
+            const response=await axios.put("http://localhost:3000/editProfile",{
+                id,
+                name: newValues.name,
+                role: newValues.role,
+                bio: newValues.bio
+            },{
+                headers:{
+                    authorization: localStorage.getItem("token"),
+                    "Content-Type": "application/json"
+                }
+            })
+            console.log(response.data);
+            if(!response.data.success){
+                console.log("Cant update");        
+            }else{
+                console.log("Updated!!");
+                setSuccess(true);
+                setTimeout(() => {
+                    onClose();
+                }, 2000);
+            }
+
+        }catch(error){
+            console.error("Server Error", error);
+        }
+    }
+
+    return(
+        <div className="fixed inset-0 bg-white flex flex-col items-center border-4 ml-[15%] mr-[25%] pb-20">
+            {success && <div className="fixed top-0 m-4 border-2 p-2 bg-green-500 text-white font-bold rounded-xl">Profile updated Successfull.</div>}
+            <button 
+                className="ml-auto mb-auto p-6 text-3xl font-extrabold border-3 cursor-pointer"
+                onClick={onClose}
+                >X
+            </button>
+            <div className="w-40 h-40 rounded-full bg-black mb-10"></div>
+            <Label
+                children="Name"
+                type="text"
+                name="name"
+                value={newValues.name}
+                onChange={handleChange}
+            />
+            <Label 
+                children="Role"
+                type="text"
+                name="role"
+                value={newValues.role}
+                onChange={handleChange}
+            />
+            <Label 
+                children="Bio"
+                type="textarea"
+                name="bio"
+                value={newValues.bio}
+                onChange={handleChange}
+            />
+            <button 
+                className="bg-red-400 text-white font-bold px-4 py-2 mt-4 rounded-sm cursor-pointer"
+                onClick={handleSubmit}>
+                    Apply Changes
+            </button>
+        </div>
+    )
+}
 
 export const Profile=()=>{
     const [loading, setLoading]=useState(true);
@@ -174,3 +269,4 @@ export const Profile=()=>{
         </>
     )
 }
+
