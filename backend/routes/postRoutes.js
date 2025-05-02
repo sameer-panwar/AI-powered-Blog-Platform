3747ba86556c1c81d027f3226aca496594bcc3a7
sameer-panwar <sameerpanwar9720@gmail.com>
2025-05-02 15:28:57 +0530

    Added comment route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { verifyToken } = require("../middlewares/authMiddleware");
-const { postBlog, updateLike, updateComment } = require("../controllers/postController");
+const { postBlog, updateLike, updateComment, getBlogs } = require("../controllers/postController");
 
 const router = express.Router();
 
+router.get("/getBlogs", verifyToken, getBlogs);
 router.post("/postBlog",verifyToken,  postBlog);
 router.put("/updateLike",verifyToken,  updateLike);
 router.put("/comment", verifyToken, updateComment);
