33683491a7a05e65e172e4397564b02ec1537d41
sameer-panwar <sameerpanwar9720@gmail.com>
2025-05-19 20:34:25 +0530

    Done with cleaning of Backend

--- backend/server.js
+++ backend/server.js
@@ -1,353 +1,341 @@
-const express=require("express");
+const express = require("express");
 const cors=require("cors");
-const jwt=require("jsonwebtoken");
-const { loginCheck, blogCheck } = require("./type");
-const { userDB, blogsDB } = require("./db");
+require('dotenv').config();
 
+const connectDB = require("./config/db")
 
 const app=express();
     
 app.use(express.json());
 app.use(cors());
 
-const secretkey="2400424";
 
-function generateToken(createPayload){
-    const token=jwt.sign({email: createPayload.email, password: createPayload.password}, secretkey);
-    return token;
-}
+connectDB();
 
-const verifyToken=async (req, res, next)=>{
-    const token=req.headers.authorization;
+app.use(require("./routes/authRoutes"));
+app.use(require("./routes/postRoutes"));
+app.use(require("./routes/userRoutes"));
 
-    if (!token) {
-        console.log("inalid token");
-        return res.status(401).json({ error: "No token provided" }); 
-    }
+// app.get('/autoLogin',verifyToken, (req, res)=>{
 
-    jwt.verify(token, secretkey,async (err, decoded)=>{
-        if(err){
-            console.log("not valid token");
-            return res.status(401).json({error: "Invalid token"})
-        }
+//     if(!req.user){
+//         return res.status(404).json({
+//             msg: "User not found"
+//         })
+//     }
 
-        req.user =await userDB.findOne({email: decoded.email});
-        next();
+//     res.status(200).json({
+//         msg: "You are logged in",
+//         success: true
+//     })
+// })
 
-    })
-}
+// app.get('/profile', verifyToken, async (req, res)=>{
 
-app.get('/autoLogin',verifyToken, (req, res)=>{
+//     const email=req.user.email;
+//     const user=await userDB.findOne({email});
+//     if(!user){
+//         return res.status(404).json({msg: "Data not Found"});
+//     }
 
-    if(!req.user){
-        return res.status(404).json({
-            msg: "User not found"
-        })
-    }
+//     res.status(200).json({msg: "Here is your profile", success: true, user})
+// })
 
-    res.status(200).json({
-        msg: "You are logged in",
-        success: true
-    })
-})
+// app.get('/adminBlogs', verifyToken, async(req, res)=>{
+//     const email=req.user.email;
 
-app.get('/profile', verifyToken, async (req, res)=>{
-
-    const email=req.user.email;
-    const user=await userDB.findOne({email});
-    if(!user){
-        return res.status(404).json({msg: "Data not Found"});
-    }
-
-    res.status(200).json({msg: "Here is your profile", success: true, user})
-})
-
-app.get('/adminBlogs', verifyToken, async(req, res)=>{
-    const email=req.user.email;
- 
-
-    const user=await userDB.findOne({email});
+//     const user=await userDB.findOne({email});
     
-    const {username} =user;
+//     const {username} =user;
 
-    const adminBlogs=await blogsDB.find({username});
+//     const adminBlogs=await blogsDB.find({username});
 
-    res.status(200).json({
-        msg: "Here are the Admin Blogs",
-        success: true,
-        adminBlogs
-    })    
-})
+//     res.status(200).json({
+//         msg: "Here are the Admin Blogs",
+//         success: true,
+//         adminBlogs
+//     })    
+// })
 
-app.post('/login',async (req, res)=>{
-    const {email, password}=req.body;
-    const user=await userDB.findOne({email, password});
+// app.post('/login',async (req, res)=>{
+//     try{
+//         const {email, password}=req.body;
+//         const user=await userDB.findOne({email: email});
+//         console.log(user);
 
+//         if(!user){
+//             return res.status(404).json({
+//                 msg: "User not Found"
+//             })
+//         }
 
-    if(!user){
-        return res.status(404).json({
-            msg: "User not Found"
-        })
-    }
+//         if(user.password !== password){
+//             return res.status(404).json({
+//                 msg: "Incorrect Password"
+//             })
+//         }
 
-    const token=generateToken({email, password});
+//         const token=generateToken({email, password});
 
-    res.status(200).json({
-        msg: "User exist. You are IN",
-        success: true,
-        token,
-        user
-    })
+//         res.status(200).json({
+//             msg: "User exist. You are IN",
+//             success: true,
+//             token,
+//             user
+//         })
+//     }catch(error){
+//         console.log("Server Error: ", error);
+//         res.status(500).json({
+//             msg: "Server Error"
+//         });
+//     }
 
-})
+// })
 
-app.post("/signup",async (req, res)=>{
-    const createPayload=req.body;
-    const parsePayload=loginCheck.safeParse(createPayload);
-    if(!parsePayload.success){
-        return res.status(403).json({
-            msg: "Invalid email or password"
-        })
-    }
+// app.post("/signup",async (req, res)=>{
+//     const createPayload=req.body;
+//     const parsePayload=loginCheck.safeParse(createPayload);
+//     if(!parsePayload.success){
+//         return res.status(403).json({
+//             msg: "Invalid email or password"
+//         })
+//     }
 
-    const token=generateToken(createPayload);
+//     const token=generateToken(createPayload);
 
-        await userDB.create({
-            username: createPayload.username,
-            email: createPayload.email,
-            password: createPayload.password
-        })
+//         await userDB.create({
+//             username: createPayload.username,
+//             email: createPayload.email,
+//             password: createPayload.password
+//         })
 
-    res.status(200).json({
-        msg: "Login Successfull", token
-    })
-})
-
-app.post("/signup/userInfo", verifyToken, async (req, res)=>{
-    const createPayload=req.body;
-    const user=await userDB.findOne({email: req.user.email});
-    if(!user){
-        return res.status(404).json({msg: "User not found"});
-    }
-
-    await userDB.findByIdAndUpdate(user._id, {
-        name: createPayload.name,
-        role: createPayload.role,
-        bio: createPayload.bio,
-        blogs: 0,
-        likes: 0
-    })
-
-    res.status(200).json({
-        msg: "User info has been updated"
-    })
-});
+//     res.status(200).json({
+//         msg: "Login Successfull", token
+//     })
+// })
 
 
-app.post("/postBlog", verifyToken ,async (req, res)=>{
-    const createPayload={
-        ...req.body,
-        keyword: Array.isArray(req.body.keywords) ? req.body.keywords : []};
+// app.post("/signup/userInfo", verifyToken, async (req, res)=>{
+//     const createPayload=req.body;
+//     const user=await userDB.findOne({email: req.user.email});
+//     if(!user){
+//         return res.status(404).json({msg: "User not found"});
+//     }
+
+//     await userDB.findByIdAndUpdate(user._id, {
+//         name: createPayload.name,
+//         role: createPayload.role,
+//         bio: createPayload.bio,
+//         blogs: 0,
+//         likes: 0
+//     })
+
+//     res.status(200).json({
+//         msg: "User info has been updated"
+//     })
+// });
+
+
+
+// app.post("/postBlog", verifyToken ,async (req, res)=>{
+//     const createPayload={
+//         ...req.body,
+//         keyword: Array.isArray(req.body.keywords) ? req.body.keywords : []};
  
-    const parsePayload=blogCheck.safeParse(createPayload);
+//     const parsePayload=blogCheck.safeParse(createPayload);
 
-    const user=await userDB.findOne({email: req.user.email});
-    if(!user){
-        return res.status(404).json({msg: "User not found"});
-    }
+//     const user=await userDB.findOne({email: req.user.email});
+//     if(!user){
+//         return res.status(404).json({msg: "User not found"});
+//     }
 
   
-    if(!parsePayload.success){
-        return res.status(403).json({
-            msg: "Blog post is missing something"
-        });
-    }
+//     if(!parsePayload.success){
+//         return res.status(403).json({
+//             msg: "Blog post is missing something"
+//         });
+//     }
 
-    await blogsDB.create({
-        title: createPayload.title,
-        content: createPayload.content,
-        keyword: createPayload.keywords,
-        name: user.name,
-        role: user.role,
-        username: user.username,
-        likes: 0,
-        likedBy: []
-    })
+//     await blogsDB.create({
+//         title: createPayload.title,
+//         content: createPayload.content,
+//         keyword: createPayload.keywords,
+//         name: user.name,
+//         role: user.role,
+//         username: user.username,
+//         likes: 0,
+//         likedBy: []
+//     })
 
-    await userDB.findByIdAndUpdate(
-        user._id,
-        {$inc:{blogs: 1}},
-        {new: true}
-    )
-    console.log("posted successfully");
-    res.status(200).json({
-        msg: "Blog has been posted"
-    })
-})
-
-app.get("/getBlogs",verifyToken, async (req, res)=>{
-    const blogs=await blogsDB.find().sort({createdAt: -1});
+//     await userDB.findByIdAndUpdate(
+//         user._id,
+//         {$inc:{blogs: 1}},
+//         {new: true}
+//     )
+//     console.log("posted successfully");
+//     res.status(200).json({
+//         msg: "Blog has been posted"
+//     })
+// })
 
 
-    if(!blogs){
-        return res.status(404).json({msg: "No blogs found"});
-    }
 
-    const blogData=[];
-    blogs.forEach(element => {
-        data=element;
-        blogData.push(data);
-    });
+// app.get("/getBlogs",verifyToken, async (req, res)=>{
+//     const blogs=await blogsDB.find().sort({createdAt: -1})
+//     .populate("comments.postedBy", '_id name')
+//     .exec();
+
+
+//     if(!blogs){
+//         return res.status(404).json({msg: "No blogs found"});
+//     }
+
+//     const blogData=[];
+//     blogs.forEach(element => {
+//         data=element;
+//         blogData.push(data);
+//     });
     
-    res.status(200).json({
-        msg: "Here are the blogs",
-        success: true,
-        data: blogData
-    })
-}
-);
+//     res.status(200).json({
+//         msg: "Here are the blogs",
+//         success: true,
+//         data: blogData
+//     })
+// }
+// );
 
 
-app.put("/editProfile", async (req, res) => {
-    try {
-        const {id, name, role, bio, username } = req.body;
+// app.put("/editProfile", async (req, res) => {
+//     try {
+//         const {id, name, role, bio, username } = req.body;
 
-        if (!name|| !bio || !role) {
-            return res.status(400).json({ msg: "All fields are required" });
-        }
+//         if (!name|| !bio || !role) {
+//             return res.status(400).json({ msg: "All fields are required" });
+//         }
 
-        const updatedUser = await userDB.findByIdAndUpdate(
-            id, 
-            { name, role, bio },
-            { new: true }
-        );
+//         const updatedUser = await userDB.findByIdAndUpdate(
+//             id, 
+//             { name, role, bio },
+//             { new: true }
+//         );
 
-        const updatedBlogs= await blogsDB.updateMany(
-            {username: username},
-            {$set: {name: name , role: role}}
-        );
+//         const updatedBlogs= await blogsDB.updateMany(
+//             {username: username},
+//             {$set: {name: name , role: role}}
+//         );
 
         
 
-        if (!updatedUser) {
-            return res.status(404).json({ msg: "User not found" });
-        }
+//         if (!updatedUser) {
+//             return res.status(404).json({ msg: "User not found" });
+//         }
 
-        res.status(200).json({
-            msg: "Profile changes have been saved",
-            success: true,
-            user: updatedUser,
-            blogs: updatedBlogs
-        });
-    } catch (error) {
-        console.error("Error updating profile:", error);
-        res.status(500).json({ msg: "Internal Server Error" });
-    }
-});
+//         res.status(200).json({
+//             msg: "Profile changes have been saved",
+//             success: true,
+//             user: updatedUser,
+//             blogs: updatedBlogs
+//         });
+//     } catch (error) {
+//         console.error("Error updating profile:", error);
+//         res.status(500).json({ msg: "Internal Server Error" });
+//     }
+// });
 
 
-app.get("/searchUser", verifyToken, async (req, res) => {
-    try {
-        const search =  req.query.searchUser;
-        console.log({serach: search});
-        if (!search) {
-            return res.status(400).json({ msg: "Search field is required" });
-        }
+// app.get("/searchUser", verifyToken, async (req, res) => {
+//     try {
+//         const search =  req.query.searchUser;
+//         console.log({serach: search});
+//         if (!search) {
+//             return res.status(400).json({ msg: "Search field is required" });
+//         }
 
-        const users = await userDB.find({ name: { $regex: search, $options: "i" } });
-        console.log(users);
+//         const users = await userDB.find({ name: { $regex: search, $options: "i" } });
+//         console.log(users);
         
-        const usersData=[];
-        users.forEach(element => {
-            console.log(element.name);
-            data= {
-                id: element._id,
-                name: element.name,
-                role: element.role
-            }
-            usersData.push(data);
-        });
+//         const usersData=[];
+//         users.forEach(element => {
+//             console.log(element.name);
+//             data= {
+//                 id: element._id,
+//                 name: element.name,
+//                 role: element.role
+//             }
+//             usersData.push(data);
+//         });
 
-        console.log(usersData);
+//         console.log(usersData);
 
-        if (!users) {
-            return res.status(404).json({ msg: "No users found" });
-        }
+//         if (!users) {
+//             return res.status(404).json({ msg: "No users found" });
+//         }
 
 
-        res.status(200).json({
-            msg: "Here are the search results",
-            success: true,
-            users: usersData
-        });
-    } catch (error) {
-        console.error("Error searching for user:", error);
-        res.status(500).json({ msg: "Internal Server Error" });
-    }
-});
+//         res.status(200).json({
+//             msg: "Here are the search results",
+//             success: true,
+//             users: usersData
+//         });
+//     } catch (error) {
+//         console.error("Error searching for user:", error);
+//         res.status(500).json({ msg: "Internal Server Error" });
+//     }
+// });
 
-app.post("/updateLike", verifyToken, async (req, res)=>{
-    const blogID=req.body.blogId;
-    const user = req.user._id;
+// app.post("/updateLike", verifyToken, async (req, res)=>{
+//     const blogID=req.body.blogId;
+//     const user = req.user._id;
     
 
-    try{
-        const blog = await blogsDB.findById(blogID);
+//     try{
+//         const blog = await blogsDB.findById(blogID);
 
-        if(!blog){
-            res.status(404).json({msg: "Blog not found"});
-        }
-        const isLiked = blog?.likedBy?.includes(user);
-
-
-        let updatedData;
-
-        if (isLiked) {
-            updatedData = await blogsDB.findByIdAndUpdate(
-                blogID,
-                {
-                $inc: { likes: -1 },
-                $pull: { likedBy: user },
-                },
-                { new: true }
-        );
-        console.log("unliked");
-        } else {
-            updatedData = await blogsDB.findByIdAndUpdate(
-                blogID,
-                {
-                $inc: { likes: 1 },
-                $push: { likedBy: user },
-                },
-                { new: true }
-            );
-        console.log("Liked");
-        }
-        console.log(updatedData);
-
-        res.status(200).json({
-            msg: "Blog is Updated!",
-            data: updatedData
-        })
-
-        
-    }catch(err){
-        console.log("Error occcured", err);
-        res.status(400).json({msg: err})
-    } 
-});
-
-app.get('/checkLike', verifyToken, async (req, res)=>{
-    
+//         if(!blog){
+//             res.status(404).json({msg: "Blog not found"});
+//         }
+//         const isLiked = blog?.likedBy?.includes(user);
 
 
-})
+//         let updatedData;
+
+//         if (isLiked) {
+//             updatedData = await blogsDB.findByIdAndUpdate(
+//                 blogID,
+//                 {
+//                 $inc: { likes: -1 },
+//                 $pull: { likedBy: user },
+//                 },
+//                 { new: true }
+//         );
+//         console.log("unliked");
+//         } else {
+//             updatedData = await blogsDB.findByIdAndUpdate(
+//                 blogID,
+//                 {
+//                 $inc: { likes: 1 },
+//                 $push: { likedBy: user },
+//                 },
+//                 { new: true }
+//             );
+//         console.log("Liked");
+//         }
+//         console.log(updatedData);
+
+//         res.status(200).json({
+//             msg: "Blog is Updated!",
+//             data: updatedData
+//         })
+
+//     }catch(err){
+//         console.log("Error occcured", err);
+//         res.status(400).json({msg: err})
+//     } 
+// });
 
 
-app.get("/getBloggg", (req, res) => {
-    res.send("get blog");
-})
+const port = process.env.PORT;
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("listening to port 3000");
 })
